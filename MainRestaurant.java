ce3297d7c1f60a2589dea8433ff16fa64f0183f8
angunnnnnn <lullaby@Lullabys-MacBook-Pro.local>
2016-03-03 15:43:54 +0700

    first commit

---
+++ MainRestaurant.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainrestaurant;
+/**
+ *
+ * @author lullaby
+ */
+public class MainRestaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main m1 = new Main();
+        m1.setJFrame(m1);
+        m1.getJFrame().setVisible(true);
+    }
+}
